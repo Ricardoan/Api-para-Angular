9b0369bd1d95596d65b7b7647da8a875b3710205
Ricardo Nascimento <ricardoan287@gmail.com>
2023-09-19 17:22:13 -0300

    Criação classe cidade

---
+++ ApiRinunci/Models/cidade.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiRinunci.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+    public class cidade
+    {
+    [Key]
+    public int cidadeId { get; set; }
+    [Required]
+    [StringLength(255)]
+    public string nomecidade { get; set; }
+}
+
